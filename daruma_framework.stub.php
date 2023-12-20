12472f6e84e6eae992b91ed3903c6bff7356af50
victordev13 <vtrcarvalho.13@gmail.com>
2023-12-20 15:09:35 -0300

    feat: stubs

---
+++ daruma_framework.stub.php
@@ -0,0 +1,11 @@
+<?php
+
+function eBuscarPortaVelocidade_DUAL_DarumaFramework(): int {}
+
+function iImprimirTexto_DUAL_DarumaFramework(string $texto, int $tam): int {}
+
+function eDefinirProduto_Daruma(string $produto): int {}
+
+function regAlterarValor_Daruma(string $pathChave, string $valor): int {}
+
+function iCFImprimir_NFCe_Daruma(string $pathXMLVenda, string $pathRetornoWS, string $linkQrCode, int $numColunas, int $tipoNF): int {}
